b4b7cf3044bba30d2d31d4b3f72d71ae16772db5
Pranjal <tataisarkar2001@gmail.com>
2023-08-11 19:30:48 +0530

    New commit. Just some changes with the code orientation!

--- src/App.js
+++ src/App.js
@@ -6,37 +6,37 @@ function App() {
 
   const [scanResult, setScanResult] = useState(null);
 
-useEffect(() => {
-  const scanner = new Html5QrcodeScanner('reader', {
-    qrbox: {
-      width: 250,
-      height:250,
-    },
-    fps: 5,
-  });
+  useEffect(() => {
+    const scanner = new Html5QrcodeScanner('reader', {
+      qrbox: {
+        width: 250,
+        height: 250,
+      },
+      fps: 5,
+    });
 
-  scanner.render(success, error);
+    scanner.render(success, error);
 
-  function success(result){
-    scanner.clear();
-    setScanResult(result);
-  }
+    function success(result) {
+      scanner.clear();
+      setScanResult(result);
+    }
 
-  function error(err) {
-    console.warn(err);
-  }
-}, []);
+    function error(err) {
+      console.warn(err);
+    }
+  }, []);
 
   return (
     <div className="App">
       <h1>QR Code Scanning in React</h1>
       {scanResult
-      ?
-      <div>Success: <a href={"http://" + scanResult}>{scanResult}</a></div>
-      :
-      <div id="reader"></div>
+        ?
+        <div>Success: <a href={"http://" + scanResult}>{scanResult}</a></div>
+        :
+        <div id="reader"></div>
       }
-      
+
     </div>
   );
 }
